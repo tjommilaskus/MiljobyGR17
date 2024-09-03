7b1623212611c9ed854c0b520d5e209030de935f
Tjommilaskus <mps.94@me.com>
2024-09-03 11:49:57 +0200

    endret navbar la til hvl logo

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar() {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="/hvl-logo.png" className="h-8" alt="Flowbite Logo" />
+          <img src="" className="h-8 hidden" alt="" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             HVLTOPIA
           </span>
